eb7f58abf876cf37cdf0e4895ece7a76c3219b41
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-08-30 03:33:16 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,39 @@
+// Task-01
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+// let applePrice = 250;
+// let orangePrice = 300;
+// let totalAmount = applePrice + orangePrice;
+// let giveTaka = 1000;
+// let exchange = giveTaka - totalAmount;
+// console.log("The shopkeeper will give me " + exchange  + " taka" );
+
+
+// Task-02
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+// let math = 75;
+// let chemistry = 90;
+// let biology = 90;
+// let total = math + chemistry + biology;
+// let average = total / 3;
+// console.log(average);
+
+// Task- 03
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+// let num = 25
+// let remainder = num % 5;
+// console.log(remainder);
+
+
+// Task-04
+// What will be the result of the following codes:
+
+
+// isNaN is a method which convert the value to a number before testing it. If a number is already in number type then result will be false and if a number is string then isNaN method convert it to a number.
+// var a = isNaN(11);
+// console.log(a);
+
+// var a = isNaN('2-10');
+// console.log(a);
